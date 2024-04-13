51430e9344b4c390cfc00063dee70bee1e516779
EmanuelMartinez23 <emanuelmartinez230801@gmail.com>
2024-04-13 16:48:07 -0600

    Se quito el codigo duplicado para mostar las ciudades

--- src/main/java/org/devemanuel/HomeFrame.java
+++ src/main/java/org/devemanuel/HomeFrame.java
@@ -61,147 +61,119 @@ public class HomeFrame  extends JFrame {
     private void initComponents() {
         CiudadDaoImpl dao =  new CiudadDaoImpl();
         List<Ciudad> ciudads = dao.obtenerTodasCiudades();
-//        List<Ciudad> ciudadsDistrito;
-        //int  indice = 0;
-        explorarTodosLosRegistrosButton.addActionListener(e-> {
-            System.out.println("Boton Explorar Todos Los Registros");
-            //List<Ciudad> ciudads = dao.obtenerTodasCiudades();
-            int totalRegistro = ciudads.size();
-            textFieldAnterior.setText("1");
-            textFieldSiguiente.setText(Integer.toString(totalRegistro));
-            Ciudad city1 = ciudads.get(indice);
-            textFieldID.setText(String.valueOf(city1.getIdentificador()));
-            textFieldNombre.setText(city1.getNombreCiudad());
-            textFieldCode.setText(city1.getCodigoCiudad());
-            textFieldDistrito.setText(city1.getDistrito());
-            textFieldPoblacion.setText(String.valueOf(city1.getPoblacion()));
-            textFieldAnterior.setText(String.valueOf(indice+1));
+        if (explorarTodosLosRegistrosButton != null) {
+            explorarTodosLosRegistrosButton.addActionListener(e-> {
+                System.out.println("Boton Explorar Todos Los Registros");
+                int totalRegistro = ciudads.size();
+                textFieldAnterior.setText("1");
+                textFieldSiguiente.setText(Integer.toString(totalRegistro));
+                mostrarCiudades(ciudads,indice);
+            });
+        }
 
-        });
+        if (anteriorButton != null) {
+            anteriorButton.addActionListener(e-> {
+                System.out.println("Boton Anterior");
+                indice = indice-1;
+                if (indice < 0){
+                    indice = 0;
+                }
+                mostrarCiudades(ciudads,indice);
+            });
+        }
 
-        anteriorButton.addActionListener(e-> {
-            System.out.println("Boton Anterior");
-            indice = indice-1;
-            if (indice < 0){
-                indice = 0;
-            }
-            
-            Ciudad city = ciudads.get(indice);
-            textFieldID.setText(String.valueOf(city.getIdentificador()));
-            textFieldNombre.setText(city.getNombreCiudad());
-            textFieldCode.setText(city.getCodigoCiudad());
-            textFieldDistrito.setText(city.getDistrito());
-            textFieldPoblacion.setText(String.valueOf(city.getPoblacion()));
-            textFieldAnterior.setText(String.valueOf(indice+1));
-
-        });
-
-        siguienteButton.addActionListener(e->{
-            System.out.println("Boton siguiente");
-            indice = indice + 1;
-            Ciudad city = ciudads.get(indice);
-            textFieldID.setText(String.valueOf(city.getIdentificador()));
-            textFieldNombre.setText(city.getNombreCiudad());
-            textFieldCode.setText(city.getCodigoCiudad());
-            textFieldDistrito.setText(city.getDistrito());
-            textFieldPoblacion.setText(String.valueOf(city.getPoblacion()));
-            textFieldAnterior.setText(String.valueOf(indice+1));
+        if (siguienteButton != null) {
+            siguienteButton.addActionListener(e->{
+                System.out.println("Boton siguiente");
+                indice = indice + 1;
+                mostrarCiudades(ciudads,indice);
+            });
+        }
 
 
-        });
+        if (buscarButton != null) {
+            buscarButton.addActionListener(e-> {
+                System.out.println("Boton buscar ");
 
+                String distrito = textFieldBuscar.getText();
+                ciudadsDistrito = dao.obtenerCiudadesPorNombre(distrito);
+                // verificamos si la lista esta vacia, si lo esta es que el distrito no esta
+                if(ciudadsDistrito.isEmpty()){
+                    verificaListBuscar.setText("No se encontro ningun registro o esta escrito incorrectamente");
+                    verificaListBuscar.setForeground(Color.RED);
+                }
+                else{
+                    verificaListBuscar.setText("");
+                    int totalRegistro = ciudadsDistrito.size();
+                    textFieldAnteriorBuscar.setText("1");
+                    textFieldSiguienteBuscar.setText(Integer.toString(totalRegistro));
+                    mostrarCiudadesDistrito(ciudadsDistrito,indiceDis);
+                }
 
-        buscarButton.addActionListener(e-> {
-            System.out.println("Boton buscar ");
+            });
+        }
 
-            String distrito = textFieldBuscar.getText();
-            ciudadsDistrito = dao.obtenerCiudadesPorNombre(distrito);
-            // verificamos si la lista esta vacia, si lo esta es que el distrito no esta
-            if(ciudadsDistrito.isEmpty()){
-                verificaListBuscar.setText("No se encontro ningun registro o esta escrito incorrectamente");
-                verificaListBuscar.setForeground(Color.RED);
-            }
-            else{
-                verificaListBuscar.setText("");
-                int totalRegistro = ciudadsDistrito.size();
-                textFieldAnteriorBuscar.setText("1");
-                textFieldSiguienteBuscar.setText(Integer.toString(totalRegistro));
-                Ciudad city1 = ciudadsDistrito.get(indiceDis);
-                textFieldBuscarID.setText(String.valueOf(city1.getIdentificador()));
-                textFieldBuscarNombre.setText(city1.getNombreCiudad());
-                textFieldBuscarCode.setText(city1.getCodigoCiudad());
-                textFieldBuscarDistrito.setText(city1.getDistrito());
-                textFieldBuscarPoblacion.setText(String.valueOf(city1.getPoblacion()));
-                textFieldAnteriorBuscar.setText(String.valueOf(indiceDis+1));
+        if (siguienteBuscarButton != null) {
+            siguienteBuscarButton.addActionListener(e -> {
+                System.out.println("Boton Buscar siguiente");
+                indiceDis = indiceDis + 1;
+                if (indiceDis >= ciudadsDistrito.size()){
+                    indiceDis = ciudadsDistrito.size()-1;
+                }
+                mostrarCiudadesDistrito(ciudadsDistrito,indiceDis);
+            });
+        }
 
-            }
+        if (anteriorBuscarButton != null) {
+            anteriorBuscarButton.addActionListener(e-> {
+                System.out.println("Boton Buscar Anterior");
+                indiceDis = indiceDis-1;
+                if (indiceDis < 0){
+                    indiceDis = 0;
+                }
+                mostrarCiudadesDistrito(ciudadsDistrito,indiceDis);
+            });
+        }
 
+        if (agregarButton != null) {
+            agregarButton.addActionListener(e-> {
+                System.out.println("Boton agregar");
+                String name = textFieldAgregarNombre.getText();
+                String code = textFieldAgregarCode.getText();
+                String distrito = textFieldAgregarDistrito.getText();
+                int poblacion = Integer.parseInt(textFieldAgregarPoblacion.getText());
+                Ciudad city = new Ciudad(name,code,distrito,poblacion);
+                boolean b = dao.agregarCiudad(city);
+                if (b){
+                    textVerifica.setText("Registro agregado correctamente");
+                    textVerifica.setForeground(Color.GREEN);
+                }
+                else {
+                    textVerifica.setText("Hubo algun error");
+                    textVerifica.setForeground(Color.RED);
+                }
 
+            });
+        }
 
-        });
-
-        siguienteBuscarButton.addActionListener(e -> {
-            System.out.println("Boton Buscar siguiente");
-            indiceDis = indiceDis + 1;
-//            System.out.println(ciudadsDistrito.size());
-            if (indiceDis >= ciudadsDistrito.size()){
-                indiceDis = ciudadsDistrito.size()-1;
-            }
-            Ciudad city = ciudadsDistrito.get(indiceDis);
-            textFieldBuscarID.setText(String.valueOf(city.getIdentificador()));
-            textFieldBuscarNombre.setText(city.getNombreCiudad());
-            textFieldBuscarCode.setText(city.getCodigoCiudad());
-            textFieldBuscarDistrito.setText(city.getDistrito());
-            textFieldBuscarPoblacion.setText(String.valueOf(city.getPoblacion()));
-            textFieldAnteriorBuscar.setText(String.valueOf(indiceDis+1));
-        });
-
-        anteriorBuscarButton.addActionListener(e-> {
-            System.out.println("Boton Buscar Anterior");
-            indiceDis = indiceDis-1;
-            if (indiceDis < 0){
-                indiceDis = 0;
-            }
-
-            Ciudad city = ciudadsDistrito.get(indiceDis);
-            textFieldBuscarID.setText(String.valueOf(city.getIdentificador()));
-            textFieldBuscarNombre.setText(city.getNombreCiudad());
-            textFieldBuscarCode.setText(city.getCodigoCiudad());
-            textFieldBuscarDistrito.setText(city.getDistrito());
-            textFieldBuscarPoblacion.setText(String.valueOf(city.getPoblacion()));
-            textFieldAnteriorBuscar.setText(String.valueOf(indiceDis+1));
-
-        });
-
-
-
-        agregarButton.addActionListener(e-> {
-            System.out.println("Boton agregar");
-            String name = textFieldAgregarNombre.getText();
-            String code = textFieldAgregarCode.getText();
-            String distrito = textFieldAgregarDistrito.getText();
-            int poblacion = Integer.parseInt(textFieldAgregarPoblacion.getText());
-            Ciudad city = new Ciudad(name,code,distrito,poblacion);
-            boolean b = dao.agregarCiudad(city);
-            if (b){
-                textVerifica.setText("Registro agregado correctamente");
-                textVerifica.setForeground(Color.GREEN);
-            }
-            else {
-                textVerifica.setText("Hubo algun error");
-                textVerifica.setForeground(Color.RED);
-            }
-
-        });
-
-        textFieldAnterior.addActionListener(e -> {
-            String textoIngresado = textFieldAnterior.getText();
-            System.out.println("Texto en textFieldAnterior: " + textoIngresado);
-        });
-
-
-
-
+    }
+    private void mostrarCiudades(List<Ciudad> list, int indice){
+        Ciudad city1 = list.get(indice);
+        textFieldID.setText(String.valueOf(city1.getIdentificador()));
+        textFieldNombre.setText(city1.getNombreCiudad());
+        textFieldCode.setText(city1.getCodigoCiudad());
+        textFieldDistrito.setText(city1.getDistrito());
+        textFieldPoblacion.setText(String.valueOf(city1.getPoblacion()));
+        textFieldAnterior.setText(String.valueOf(indice+1));
+    }
+    private void mostrarCiudadesDistrito(List<Ciudad> list, int indice){
+        Ciudad city = list.get(indice);
+        textFieldBuscarID.setText(String.valueOf(city.getIdentificador()));
+        textFieldBuscarNombre.setText(city.getNombreCiudad());
+        textFieldBuscarCode.setText(city.getCodigoCiudad());
+        textFieldBuscarDistrito.setText(city.getDistrito());
+        textFieldBuscarPoblacion.setText(String.valueOf(city.getPoblacion()));
+        textFieldAnteriorBuscar.setText(String.valueOf(indice+1));
     }
 
 
